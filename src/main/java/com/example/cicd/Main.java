fbe2b1f05457299273865f4418b46d774863c6a5
Mihran_Maruqyan <Mihran_Maruqyan@epam.com>
2023-09-23 22:08:27 +0400

    adds CI/CD topic

--- src/main/java/com/example/cicd/Main.java
+++ src/main/java/com/example/cicd/Main.java
@@ -2,22 +2,66 @@ package com.example.cicd;
 
 public class Main {
 
-    //CONTINUES INTEGRATION
+    //HOW CI/CD WORKS
+    //CI/CD is an automated process that involves frequent code integration, automated testing, and continuous deployment of software changes to production.
 
-    //erb developer-@ ashxatume codi vra local environment-um
-    //ev cmmit pushe anum repository
-    //ays cod|@ arden karoxe integrvel goyutyun unecox codi het
-    //cod@ testavorvum e erkar arka code-i het miasin ev stugvumm en error ner@
-    //Ogtagorcelov ays method@ developer@ karoxe shat arag gtnel ev lucel xndir@
+    //The four steps of a CI/CD pipeline work together to ensure that software changes are tested, integrated, and deployed to production automatically.
+    //This automation helps to reduce errors, increase efficiency, and improve the overall quality of the software.
 
-    //skzbum developer@ commit pushe anum cod@ depi git aynuhetev jenkinsi mijocov arden isk config arac
-    //pipeline -i mijocov ashxatumen avtomatacvac tester@ .
+    //By adopting a CI/CD pipeline, development teams can achieve faster release cycles, reduce the risk of software defects, and improve the user experience.
 
-    //CONTINUES DEPLOYMENT
-    //sa nuyn banne inj CI-@ uxxaki verjum deploya anum depi production
-    //deploy anel@ depi production katarvume avtomat
+    //Keep in mind that the pipeline stages might look different given the specific project or company we're talking about.
+    //Meaning, some teams might or might not use automated testing, some teams might or might not have a "staging" environment, and so on.
 
-    //CONTINUES DELIVERY
-    //sa nuyn Continues Deploymentne uxxaki depi production deploy anel@ pipelin-@ dadarecnume ashxatanq@ ev developer@ manual e deploy anum
+    //******************************************************************************************************************
+
+    //                                  LET'S EXPLAIN EACH STEP IN A LITTLE MORE DETAIL:
+
+    //1- CODE INTEGRATION
+    //The first step in the CI/CD pipeline is code integration.
+    //In this step, developers commit their code changes to a remote repository (like GitHub, GitLab or BitBucket), where the code is integrated with the main codebase.
+    //This step aims to ensure that the code changes are compatible with the rest of the codebase and do not break the build.
+
+    //******************************************************************************************************************
+
+    //2 - AUTOMATED TESTING
+    //Once the code is integrated, the next step is automated testing.
+    //Automated testing involves running a suite of tests to ensure that the code changes are functional, meet the expected quality standards, and are free of defects.
+    //This step helps identify issues early in the development process, allowing developers to fix them quickly and efficiently.
+
+    //******************************************************************************************************************
+
+    //3-CONTINUES DEPLOYMENT
+    //After the code changes pass the automated testing step, the next step is continuous deployment.
+    //In this step, the code changes are automatically deployed to a staging environment for further testing.
+    //This step aims to ensure that the software is continuously updated with the latest code changes, delivering new features and functionality to users quickly and efficiently.
+
+    //4-PRODUCTION DEPLOYMENT
+    //The final step in the CI/CD pipeline is production deployment.
+    //In this step, the software changes are released to end-users.
+    //This step involves monitoring the production environment, ensuring that the software is running smoothly, and identifying and fixing any issues that arise.
+
+    //******************************************************************************************************************
+
+    //                            THE KEY BENEFITS OF CI/CD
+
+    //THE KEY BENEFITS OF CI/CD INCLUDES `
+
+    //1- FASTER RELEASE CYCLES
+    //By automating the testing and deployment process, CI/CD enables teams to release software more frequently, responding quickly to customer needs.
+
+    //2- IMPROVED QUALITY
+    //Automated testing ensures that software changes do not introduce new bugs or issues, improving the overall quality of the software.
+
+    //3- INCREASED COLLABORATION
+    //Frequent code integration and testing require developers to work closely together, leading to better collaboration and communication.
+
+    //4- REDUCED RISK
+    // Continuous deployment allows developers to identify and fix issues quickly, reducing the risk of major failures
+
+    //5- COST-EFFECTIVE
+    //CI/CD reduces the amount of manual work required to deploy software changes, saving time and reducing costs.
+
+    //******************************************************************************************************************
 
 }
