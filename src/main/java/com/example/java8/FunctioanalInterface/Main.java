e0ff1a66505e78815127c8e7ca604a44152141bc
Sevak Martirosyan <sm@payneteasy.com>
2025-04-10 15:20:53 +0400

    fix

--- src/main/java/com/example/java8/FunctioanalInterface/Main.java
+++ src/main/java/com/example/java8/FunctioanalInterface/Main.java
@@ -3,16 +3,12 @@ package com.example.java8.FunctioanalInterface;
 import java.util.function.Predicate;
 
 public class Main {
-
     public static void main(String[] args) {
 
         //functional interface example
         //Provides more suitable syntax
         Runnable runnable = () -> System.out.println("thread staretd");
         runnable.run();
-
-
-
 
         //This situation is created anonymous class
         Runnable runnable1 = new Runnable() {
@@ -26,7 +22,5 @@ public class Main {
         //Predicate functional interface returns logical value
         Predicate<Integer> predicate = t-> t==0;
         System.out.println(predicate.test(10));
-
-
     }
 }
